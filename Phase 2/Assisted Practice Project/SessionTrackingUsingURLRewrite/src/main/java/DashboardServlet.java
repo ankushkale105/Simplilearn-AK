819be784c7ede96820dc357fcc8ec02b1d0b14e2
ankushkale105 <97828779+ankushkale105@users.noreply.github.com>
2022-02-22 18:28:18 +0530

    Assisted Practice Project

---
+++ Phase 2/Assisted Practice Project/SessionTrackingUsingURLRewrite/src/main/java/DashboardServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/DashboardServlet")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        
+        String userId = request.getParameter("userid");
+        if (userId == null ) {
+            out.println("No UserId was found in the URL.<br>");
+        } else {
+            out.println("UserId obtained from URL Rewriting : " + userId + "<br>");
+        }
+        out.println("</body></html>");
+
+    }
+
+}
---
+++ Phase 2/Assisted Practice Project/SimpleJSPFile/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"     pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Simple JSP File</title>
+</head>
+<body>
+<h2>Creating a Simple JSP File</h2><hr>
+<%
+        out.println("<h1><font color='grey'>Welcome to my first JSP page.</h1>");
+%>
+</body>
+</html>
